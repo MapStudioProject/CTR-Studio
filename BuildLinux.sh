a28b2dbb02b2b70f625a3d5a4da394df7c001316
throwaway-nolsh <145593137+throwaway-nolsh@users.noreply.github.com>
2025-10-25 12:53:59 +0000

    make buildlinux way better
    applied workarounds to recent build errors and structured the commands better

--- BuildLinux.sh
+++ BuildLinux.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-pwd
+WORKDIR="$(pwd)"
+echo "Working Directory: $WORKDIR"
 
-dotnet build --runtime linux-x64 --self-contained "$(pwd)/CTR Studio"
-cp -R "$(pwd)/Plugins" "$(pwd)/CTR Studio/bin/Debug/net6.0/linux-x64/"
-cp -R "$(pwd)/CTR Studio/bin/Debug/net6.0/Plugins" "$(pwd)/CTR Studio/bin/Debug/net6.0/linux-x64/"
+echo "Copying CtrLibrary into bin (else build fails)..."
+mkdir -p "$WORKDIR/CTR Studio/bin/Debug/net6.0/linux-x64/"
+cp -R "$WORKDIR/Plugins" "$WORKDIR/CTR Studio/bin/Debug/net6.0/linux-x64/"
+echo "Applying xcopy workaraound to CTR Studio.csproj"
+sed -i 's|<Exec Command="xcopy.*|<Exec Command="cp ../Plugins/CtrLibrary/bin/Debug/net6.0 ../CTR\\ Studio/bin/Debug/net6.0/Plugins -r" />|' "$WORKDIR/CTR Studio/CTR Studio.csproj"
 
-echo "Built CTR Studio correctly! Run './CTR\ Studio/bin/Debug/net6.0/linux-x64/CTR\ Studio' to launch!"
+echo "Building now..."
+if ! dotnet build --runtime linux-x64 --self-contained "$WORKDIR/CTR Studio"; then
+    echo "Build failed! Please look at the errors provided and open an issue if you can't solve them."
+    exit 1;
+fi
+
+cp -R "$WORKDIR/CTR Studio/bin/Debug/net6.0/Plugins" "$WORKDIR/CTR Studio/bin/Debug/net6.0/linux-x64/" && echo "Success! Run './CTR\\ Studio/bin/Debug/net6.0/linux-x64/CTR\\ Studio' to launch!" || echo "Something went wrong!"
